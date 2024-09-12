d1bf7609930870dbffa0fb0a0bfbaf983dc88269
Kauany Costa <kk77665544jcmll@gmail.com>
2024-09-12 12:27:46 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,16 +2,10 @@ numero = int(input("digite um numero: "))
 def e_par(numero):
     if numero % 2 == 0:
         return True
+        print("Esse numero é par")
     else:
         return False
-    
-    if e_par(numero):
-        print("o numero é par")
-
-    else:
-        print("O numero é impar")
-       
-
+        print("Esse numero é impar")
     
 
 
