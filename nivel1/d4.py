2480210829e0e37e20f52899bbf51c6aad7ae7c5
Kauany Costa <107109137+kajcosta@users.noreply.github.com>
2024-09-24 22:37:18 -0300

    organizando na primeira pasta

---
+++ nivel1/d4.py
@@ -0,0 +1,6 @@
+#escreva um codigo que informe o antecessor e sucessor de um numero 
+numero=int(input('Escreva um numero: '))
+ant=numero-1
+suc=numero+1
+print('O numero antecessor é: ', ant)
+print('O numero sucessor é:', suc)
