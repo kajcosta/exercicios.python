f8f9c064bebe0fc7a3d0670049f32ef904b24fdd
Kauany Costa <107109137+kajcosta@users.noreply.github.com>
2024-10-03 14:34:22 -0300

    Create d6.py

---
+++ nivel1/d6.py
@@ -0,0 +1,7 @@
+#desenvolva um programa que leia 4 notas de um aluno e mostra a media 
+nota1=int(input('primeira nota: '))
+nota2=int(input('segunda nota: '))
+nota3=int(input('terceira nota: '))
+nota4=int(input('quarta nota: '))
+media=(nota1+nota2+nota3+nota4)/4
+print('a sua media foi{}'.format(media))
