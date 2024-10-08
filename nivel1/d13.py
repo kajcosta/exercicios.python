00ffdd03d283baab8b8df3cb5f856404f613b00b
Kauany Costa <107109137+kajcosta@users.noreply.github.com>
2024-10-07 22:27:43 -0300

    .....

--- nivel1/d13.py
+++ nivel1/d13.py
@@ -1,4 +1,4 @@
 #faça um codigo para transformar graus celsius em fahrenheit
-grausC= format(input('temperaturs em celsius: '))
+grausC= float(input('temperaturs em celsius: '))
 grausF=grausC *1.8 + 32
 print('Em fahrenheit equivale a {} graus '.format(grausF))
