1ff42a0cb3f156455337e752cde2f55101c7b2ad
Kauany Costa <107109137+kajcosta@users.noreply.github.com>
2024-10-10 22:44:22 -0300

    ...

--- nivel1/d014.py
+++ nivel1/d014.py
@@ -1,5 +1,5 @@
 #: Escreva um programa que pergunte a quantidade de Km percorridos por um carro alugado e a quantidade de dias pelos quais ele foi alugado. Calcule o preço a pagar, sabendo que o carro custa R$60 por dia e R$0,15 por Km rodado.
 km=float(input('quantidade de km percorrida: '))
 dias=int(input('quantidade de dias: '))
-aPagar=((km*0.15)+ (dias*60))
-print('você tera que pagar {}'.format(aPagar))
+aPagar=(km*0.15)+ (dias*60)
+print('você tera que pagar R${:.2f}'.format(aPagar))
---
+++ nivel1/d015.py
@@ -0,0 +1,4 @@
+from math import floor
+#crie um programa que leia um numero real e  mostree sua porção inteira 
+num=float(input('escreva um numero real: '))
+print(' a parte inteira é  igual a {:.1f}'.format(floor(num)))
